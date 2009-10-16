0f94f8c30511ca3ad9ae4d29cdbbfe39b0756418
Dwight Spencer <denzuko@users.sf.net>
2009-10-15 20:28:55 -0600

    Added capistrano tasks for deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,13 @@
 set :application, "Jekyll-Test-Site"
-set :repository,  "git://github.com/denzuko/{:application}"
+set :repository,  "git@github.com/denzuko/{:application}.git"
 
 set :scm, :git
+set :scm_passhprase, "" # not posting to the public
+set :user, "denzuko"
+set :branch, "master"
+set :deploy_via, :remote_cache
 
-role :web, "your web-server here"      # Your HTTP server
+role :web, "denzuko.dyndns.com"      # Your HTTP server
 
 namespace :deploy do
   task :publish do
--- tasks/publish.rake
+++ tasks/publish.rake
@@ -1,5 +1,3 @@
-require 'rubygems'
-
 task :publish do
   system 'copy _site/ D:\My Dropbox\Applications\xampp\xampplite\htdocs\blog'
 end
